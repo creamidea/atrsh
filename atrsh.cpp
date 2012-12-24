328c53a174be483ea047f857d1b8013c36af9aae
Allen Zhong <allen@aspro-vm-xubuntu.(none)>
2012-12-24 16:55:06 +0800

    init main file

---
+++ atrsh.cpp
@@ -0,0 +1,9 @@
+/* This file is part of atrsh, A shell written in C++
+ * By: Allen Zhong (AstroProfundis) <allen@mail.atr.me>
+ * File Created: Dec 24, 2012
+ * Last Changed: Dec 24, 2012
+ * License: GNU General Public License v2.0
+ */
+
+#inlcude <iostream>
+#inlcude <string>
