e30188dca01bf4f5266445ebaba81183ee560c1d
AstroProfundis <zbl3041992@gmail.com>
2012-12-29 14:57:42 +0800

    a little cleanup

--- Execute.cpp
+++ Execute.cpp
@@ -1,7 +1,7 @@
 /* This file is part of atrsh, A shell written in C++
  * By: Allen Zhong (AstroProfundis) <allen@mail.atr.me>
  * File Created: Dec 28, 2012
- * Last Changed: Dec 28, 2012
+ * Last Changed: Dec 29, 2012
  * License: GNU General Public License v2.0
  */
 
--- atrsh.cpp
+++ atrsh.cpp
@@ -1,7 +1,7 @@
 /* This file is part of atrsh, A shell written in C++
  * By: Allen Zhong (AstroProfundis) <allen@mail.atr.me>
  * File Created: Dec 24, 2012
- * Last Changed: Dec 28, 2012
+ * Last Changed: Dec 29, 2012
  * License: GNU General Public License v2.0
  */
 
@@ -33,22 +33,9 @@ int main(int argc, char *argv[]){
         Execute exe;
         string cmd_path;
         if (exe.ifCmdExist(cmd_arry[0], fs, cmd_path)){
-            //pid_t pid;
-            //static pid_t exe_pid;
-            //if (pid = fork() == 0){
-                //exe_pid = getpid();
                 cout << endl;
                 exe.executeCmd(cmd_path, cmd_arry);
                 cout << endl;
-                //cout << fs.getCurrDir() << "#";
-                //kill(exe_pid, SIGCONT);
-            //} else if (pid = fork() < 0){
-            //    cout << "Execute ERROR." << endl;
-            //    return -1;
-            //} else {
-            //    exe_pid = getpid();
-                //pause();
-            //}
         } else {
             cout << "Command not Found." << endl;
         }
